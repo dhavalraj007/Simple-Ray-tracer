3539cf1a96434a36c50681740181b4e960c51690
dhavalraj007 <dhavalraj007@users.noreply.github.com>
2021-06-30 17:26:32 +0530

    added Camera.h,ray.h added glm maths library(!) moved constants to helper.h and made them inline (C++17 feature to use them anywhere w/o multiple definition errors / although not tested) added shootRay method + some Refactoring

--- Simple-Ray-tracer/Source.cpp
+++ Simple-Ray-tracer/Source.cpp
@@ -1,41 +1,50 @@
 #include<iostream>
+
 #include"stb_image.h"
 #include"stb_image_write.h"
 #include"glm/vec3.hpp"
+#include"glm/glm.hpp"
+#include"ray.h"
 #include"helper.h"
-
-// Constants
-constexpr auto SCR_WIDTH = 800;
-constexpr auto SCR_HEIGHT = 600;
-constexpr auto SCR_NC = 3;
+#include"Camera.h"
 
 
-void render()
+
+//exports file image , encompasses pixel loop
+void render(Camera&);
+//shoots ray and get backs the color of the object it hits
+color shoot_Ray(const ray&);
+
+int main()
+{
+	Camera camera(point(0, 0, 0), 400.0, 1.0);
+	render(camera);
+	return 0;
+}
+
+
+void render(Camera& camera)
 {
 	unsigned char* image = new unsigned char[SCR_WIDTH * SCR_HEIGHT * SCR_NC];
 
 	unsigned int index = 0;
-	for (int y = SCR_HEIGHT-1; y >=0; y--)
+	for (int y = SCR_HEIGHT - 1; y >= 0; y--)
 	{
 		std::cerr << "\rScan lines Remaining : " << y << "   " << std::flush;
 		for (int x = 0; x < SCR_WIDTH; x++)
 		{
-			float r = (float)x / SCR_WIDTH;
-			float g = (float)y / SCR_HEIGHT;
-			float b = 0.2f;
-			writeColor(image, index, color(r, g, b));
+			color col = shoot_Ray(camera.getRay(x, y));
+			writeColor(image, index, col);	//takes colors in 0...1
 		}
 	}
 	std::cerr << "\nDone.\n" << std::flush;
 	stbi_write_png("./shit.png", SCR_WIDTH, SCR_HEIGHT, SCR_NC, image,
-		SCR_NC *SCR_WIDTH );
+		SCR_NC * SCR_WIDTH);
 
 }
 
-
-
-int main()
+color shoot_Ray(const ray& r)
 {
-	render();
-	return 0;
+	double a{ 0.5 * (glm::normalize(r.dir).y + 1.0) };
+	return (1 - a) * glm::highp_dvec3(1.0, 1.0, 1.0) + (a)*glm::highp_dvec3(0.5, 0.7, 1.0);
 }
--- Simple-Ray-tracer/helper.h
+++ Simple-Ray-tracer/helper.h
@@ -1,8 +1,15 @@
 #pragma once
 #include"glm/vec3.hpp"
-
+#include<iostream>
 using color = glm::vec3;
+using point = glm::vec3;
+using dpoint = glm::highp_dvec3;
 
+// Constants
+inline constexpr auto SCR_ASPECT_RATIO = 16.0 / 9.0;
+inline constexpr unsigned int SCR_WIDTH = 720;
+inline constexpr unsigned int SCR_HEIGHT = SCR_WIDTH/SCR_ASPECT_RATIO;
+inline constexpr auto SCR_NC = 3;
 
 /// <summary>
 ///	writes color RGB to image pointer at index,index+1,index+2 also modifies index to index+3
